8b16cdb3d25c3d71271b8ab6d22d99611e810ea1
Michal Ozaniak <ozaniak.michal@gmail.com>
2014-08-10 16:56:26 +0200

    Added conciege image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,9 @@
 //= require jquery
 //= require bootstrap.min
+
+$(document).ready(function(){
+    $('.screen-height-element').css('height', $(window).height());
+});
+$(document).ready(function(){
+    $('.screen-height-without-navbar-element').css('height', $(window).height()-100);
+});
