48a549d1f84ae4d6fb07e9b660a9c133e05c98a0
Michal Ozaniak <ozaniak.michal@gmail.com>
2014-08-10 23:39:13 +0200

    Welcome page is finished, added skeleton for other pages.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,13 @@ $(document).ready(function(){
 $(document).ready(function(){
     $('.screen-height-without-navbar-element').css('height', $(window).height()-100);
 });
+
+$(document).ready(function(){
+    var $root = $('html, body');
+    $('a').click(function() {
+        $root.animate({
+            scrollTop: $( $.attr(this, 'href') ).offset().top
+        }, 500);
+        return false;
+    });
+});
